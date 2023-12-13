c7737560f1e2acf128dbeadab11b59cd238883ef
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-13 23:51:44 +0200

    Delivered empty items array on 200 HTTP response with JSON empty list

--- InstagramCloneFeedTests/RemoteFeedLoaderTests.swift
+++ InstagramCloneFeedTests/RemoteFeedLoaderTests.swift
@@ -76,11 +76,14 @@ class RemoteFeedLoaderTests: XCTestCase {
 //        XCTAssertEqual(capturedErrors, [.invalidData])
     }
     
-//    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
-//        let (sut, client) = makeSUT()
-//
-//        var capture
-//    }
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+        var capturedResults: [RemoteFeedLoader.Result] = []
+        sut.load { capturedResults.append($0) }
+        let emptyListJSON = Data("{\"items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyListJSON)
+        XCTAssertEqual(capturedResults, [.success([])])
+    }
     
     // MARK: - Helpers
     
