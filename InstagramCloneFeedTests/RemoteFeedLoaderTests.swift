7e663d1b899e6fecb0890a80974675ba90e37371
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-14 14:45:38 +0200

    Replaced hardcoded JSON with helper method

--- InstagramCloneFeedTests/RemoteFeedLoaderTests.swift
+++ InstagramCloneFeedTests/RemoteFeedLoaderTests.swift
@@ -80,7 +80,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
         let (sut, client) = makeSUT()
         expect(sut, toCompleteWith: .success([]), when: {
-            let emptyListJSON = Data("{\"items\": []}".utf8)
+            let emptyListJSON = makeItemsJSON([]) // Data("{\"items\": []}".utf8)
             client.complete(withStatusCode: 200, data: emptyListJSON)
         })
 //        var capturedResults: [RemoteFeedLoader.Result] = []
