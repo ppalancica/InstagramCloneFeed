bdee0aca1b8ce3a93eef7e988a0e20d185cfff6e
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-14 15:54:58 +0200

    Added memory leaks detection

--- InstagramCloneFeed/Feed API/RemoteFeedLoader.swift
+++ InstagramCloneFeed/Feed API/RemoteFeedLoader.swift
@@ -30,7 +30,13 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
                 case let .success(data, response):
-                    completion(self.map(data, from: response))
+//                    completion(self.map(data, from: response))
+                    do {
+                        let items = try FeedItemsMapper.map(data, response)
+                        completion(.success(items))
+                    } catch {
+                        completion(.failure(.invalidData))
+                    }
                 case .failure:
                     completion(.failure(.connectivity))
             }
