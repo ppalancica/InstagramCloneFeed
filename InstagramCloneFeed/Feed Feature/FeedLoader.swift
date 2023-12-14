293efea440bbde710a01420f3b77266947d32110
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-14 20:12:59 +0200

    Make the 'RemoteFeedLoader' conform to the 'FeedLoader' protocol

--- InstagramCloneFeed/Feed Feature/FeedLoader.swift
+++ InstagramCloneFeed/Feed Feature/FeedLoader.swift
@@ -15,6 +15,6 @@ public enum LoadFeedResult<Error: Swift.Error> {
 extension LoadFeedResult: Equatable where Error: Equatable {}
 
 protocol FeedLoader {
-    associatedtype Erorr: Swift.Error
+    associatedtype Error: Swift.Error
     func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
 }
