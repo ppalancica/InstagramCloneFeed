118422030cc5b03639733b3043fabbddb6ba24be
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-14 21:09:10 +0200

    Made the FeedLoader protocol public

--- InstagramCloneFeed/Feed Feature/FeedLoader.swift
+++ InstagramCloneFeed/Feed Feature/FeedLoader.swift
@@ -12,8 +12,6 @@ public enum LoadFeedResult {
     case failure(Error)
 }
 
-//extension LoadFeedResult: Equatable where Error: Equatable {}
-
-protocol FeedLoader {
+public protocol FeedLoader {
     func load(completion: @escaping (LoadFeedResult) -> Void)
 }
