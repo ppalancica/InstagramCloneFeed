c8a904f352b56b03d8b9a8601e111a1db0577661
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-16 23:06:26 +0200

    Creates URLSession data task with URL on 'URLSessionHTTPClient.get(from:URL)'

---
+++ InstagramCloneFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -0,0 +1,49 @@
+//
+//  URLSessionHTTPClientTest.swift
+//  InstagramCloneFeedTests
+//
+//  Created by Pavel Palancica on 16.12.2023.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private var session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in
+            
+        }
+    }
+}
+
+class URLSessionHTTPClientTest: XCTestCase {
+    
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "https://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs: [URL] = []
+        
+        override func dataTask(
+            with url: URL,
+            completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
+        ) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+        
+        private class FakeURLSessionDataTask: URLSessionDataTask {}
+    }
+}
