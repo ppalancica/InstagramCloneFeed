f68d8e0efa9ba81b27fdac9aa6112c96043ec9b2
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-21 23:41:04 +0200

    Does not delete cache upon 'LocalFeedLoader' creation

---
+++ InstagramCloneFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,27 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  InstagramCloneFeedTests
+//
+//  Created by Pavel Palancica on 21.12.2023.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+}
