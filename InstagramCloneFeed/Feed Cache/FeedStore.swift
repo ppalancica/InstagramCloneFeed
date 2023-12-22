3bf8950a8a3f68f9662705de593568c807dbaf92
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-22 20:25:08 +0200

    Moved FeedStore to its own file

---
+++ InstagramCloneFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,18 @@
+//
+//  FeedStore.swift
+//  InstagramCloneFeed
+//
+//  Created by Pavel Palancica on 22.12.2023.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem],
+                timestamp: Date,
+                completion: @escaping InsertionCompletion)
+}
