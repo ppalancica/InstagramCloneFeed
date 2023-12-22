87230ae31ba3a49bcc2c21a587316feacae52bf4
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-23 01:40:45 +0200

    Load command delivers no images on more tha seven days old cache

--- InstagramCloneFeedTests/Feed Cache/LoadFromCacheUseCaseTests.swift
+++ InstagramCloneFeedTests/Feed Cache/LoadFromCacheUseCaseTests.swift
@@ -63,6 +63,17 @@ class LoadFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    func test_load_deliversNoImagesOnMoreThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1) // - 7 days - 1 second
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: feed.local, timestamp: moreThanSevenDaysOldTimestamp)
+        })
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,
