b1b8a37203a670bd37477163078e37cc34758905
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-22 21:53:02 +0200

    Moved LocalFeedItem to its own file

---
+++ InstagramCloneFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,25 @@
+//
+//  LocalFeedItem.swift
+//  InstagramCloneFeed
+//
+//  Created by Pavel Palancica on 22.12.2023.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID,
+                description: String?,
+                location: String?,
+                imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
