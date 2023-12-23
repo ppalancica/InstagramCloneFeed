c82131b2c9878a2e6da8c989c608b7f2103a209f
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-23 18:39:09 +0200

    Load command deletes seven days old cache on retreival

--- InstagramCloneFeed/Feed Cache/LocalFeedLoader.swift
+++ InstagramCloneFeed/Feed Cache/LocalFeedLoader.swift
@@ -37,11 +37,14 @@ public final class LocalFeedLoader {
         store.retrieve { [unowned self] result in
             switch result {
                 case let .failure(error):
-                    self.store.deleteCachedFeed { _ in } 
+                    self.store.deleteCachedFeed { _ in }
                     completion(.failure(error))
                 case let .found(feed, timestamp) where self.validate(timestamp):
                     completion(.success(feed.toModels()))
-            case .found, .empty:
+                case .found:
+                    self.store.deleteCachedFeed { _ in }
+                    completion(.success([]))
+                case .empty:
                     completion(.success([]))
             }
         }
--- InstagramCloneFeedTests/Feed Cache/LoadFromCacheUseCaseTests.swift
+++ InstagramCloneFeedTests/Feed Cache/LoadFromCacheUseCaseTests.swift
@@ -104,6 +104,18 @@ class LoadFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_load_deleteCacheOnSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: feed.local, timestamp: sevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,
