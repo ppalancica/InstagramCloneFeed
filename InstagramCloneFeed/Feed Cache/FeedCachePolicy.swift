10ac75cb8000b5b3efca3b03d2ac45a557b54c3c
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-28 01:51:42 +0200

    Moved 'FeedCachePolicy' to its own file

---
+++ InstagramCloneFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,27 @@
+//
+//  FeedCachePolicy.swift
+//  InstagramCloneFeed
+//
+//  Created by Pavel Palancica on 28.12.2023.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private init() {}
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day,
+                                              value: maxCacheAgeInDays,
+                                              to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
