f592a253c8815edadd61257f5f283f9edb19e03f
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-28 23:53:04 +0200

    Added helpers methods to provide documentation, context and clarify test 'setUp' and 'tearDown' intent regarding side-effects

--- InstagramCloneFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ InstagramCloneFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -73,13 +73,13 @@ class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        setupEmptyStoreState()
     }
     
     override func tearDown() {
         super.tearDown()
         
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        undoStoreSideEffects()
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -160,5 +160,17 @@ class CodableFeedStoreTests: XCTestCase {
             .urls(for: .documentDirectory, in: .userDomainMask)
             .first!
             .appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+        
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 }
