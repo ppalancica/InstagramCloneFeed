d677a2b9ef825ad0a6a8a57f8bb28e1d5c8660ca
Pavel Palancica <pavel.palancica@gmail.com>
2023-12-28 23:35:59 +0200

    Added memory leak trakcing

--- InstagramCloneFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ InstagramCloneFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -146,7 +146,10 @@ class CodableFeedStoreTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT() -> CodableFeedStore {
-        return CodableFeedStore()
+    private func makeSUT(file: StaticString = #file, // #filePath
+                         line: UInt = #line) -> CodableFeedStore {
+        let sut = CodableFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
     }
 }
